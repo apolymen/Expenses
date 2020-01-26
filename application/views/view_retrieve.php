9082dcfebc02c88f391b672a88fc8c4280e78213
apolymen <apolymen@gmail.com>
2020-01-26 23:01:37 +0200

    Search functionality testing

--- application/views/view_retrieve.php
+++ application/views/view_retrieve.php
@@ -3,112 +3,121 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Expenses Home</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Expenses Home</title>
 
-	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
 
-	<style type="text/css">
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-	.top-buffer {
-		margin-top:10px;
-	}
-	</style>
+  <style type="text/css">
+  p.footer {
+    text-align: right;
+    font-size: 11px;
+    padding: 0 10px 0 10px;
+    margin: 20px 0 0 0;
+  }
+  .top-buffer {
+    margin-top:10px;
+  }
+  </style>
 
 </head>
 <body>
 
-    <div class="container">
+  <div class="container">
 
-	<div class="row top-buffer">
-	<div class="col-xs-1">
-		 <button type="button" class="btn btn-primary btn-lg" onclick="location.href='<?php echo base_url(); ?>input'">Input</button>
-	</div>
-	</div>
-	<div class="top-buffer"></div>
+    <div class="row top-buffer">
+      <div class="col-xs-2">
+        <button type="button" class="btn btn-primary btn-lg" onclick="location.href='<?php echo base_url(); ?>input'">Input</button>
+      </div>
+      <?php 
+      $attributes = array("class" => "form-horizontal", "id" => "searchform", "name" => "searchform");
+      echo form_open('main/search', $attributes); ?>
+      <div class="col-xs-4">
+          <input type="text" class="form-control" id="search" name="search" placeholder="Search description" value="<?php echo set_value('book_name'); ?>" />
+      </div>
+      <div class="col-xs-6">
+        <input type="submit" class="btn btn-success" value="Search" />
+        <input type="button" class="btn btn-danger" onclick="location.href='<?php echo base_url(); ?>main/last_page'" value="Show all" />
+      </div>
+      <?php echo form_close(); ?>
+    </div>
+    <div class="top-buffer"></div>
 
-    <p style="font-size: 14px;">Total entries: <?=$rows?></p>
+      <p style="font-size: 14px;">Total entries: <?=$rows?></p>
 
-	<table class="table table-bordered table-condensed table-striped">
-	    <thead>
-	      <tr>
-		<th>Date</th>
-		<th>Amount</th>
-		<th>Person</th>
-		<th>Description</th>
-		<th>Payment</th>
-		<th>Category</th>
-	      </tr>
-	    </thead>
-	    <tbody>
+    <table class="table table-bordered table-condensed table-striped">
+      <thead>
+        <tr>
+          <th>Date</th>
+          <th>Amount</th>
+          <th>Person</th>
+          <th>Description</th>
+          <th>Payment</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($expenses as $row): ?>
+        <tr> <td class="hidden"><?=$row->id?></td> <td style="white-space: nowrap;"><?=implode('-', array_reverse(explode('-', $row->Date)))?></td> <td><?=number_format($row->Amount,2)?></td> <td><?=$row->Person?></td> <td><?=$row->Description?></td> <td><?=$row->Method?></td> <td><?=$row->Category?></td> </tr>
+        <?php endforeach; ?>
+      </tbody>
+    </table>
 
-	    <?php foreach ($expenses as $row): ?>
-			<tr> <td class="hidden"><?=$row->id?></td> <td style="white-space: nowrap;"><?=implode('-', array_reverse(explode('-', $row->Date)))?></td> <td><?=number_format($row->Amount,2)?></td> <td><?=$row->Person?></td> <td><?=$row->Description?></td> <td><?=$row->Method?></td> <td><?=$row->Category?></td> </tr>
-	    <?php endforeach; ?>
-
-	    </tbody>
-	</table>
-
-	<div class="row">
-		<div class="col-md-12">
-			<?php echo $pagination; ?>
-		</div>
-	</div>
-
-    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+    <div class="row">
+      <div class="col-md-12">
+        <?php echo $pagination; ?>
+      </div>
     </div>
 
-    <script src="<?php echo base_url(); ?>assets/js/jquery-2.2.0.min.js"></script>
-    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
-	<script src="<?php echo base_url(); ?>assets/js/bootbox.min.js"></script>
+    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+  </div>
 
-	<script>
-	$(document).ready(function() {
-		$('tbody tr').click( function() {
-			var id = $(this).children('td').html();
-			bootbox.dialog( {
-				title: 'Confirm',
-				message: 'Edit this entry?',
-				size: 'small',
-				animate: 'false',
-				onEscape: 'true',
-				buttons: {
-					OK: {
-						label: 'OK',
-						className: "btn-primary",
-						callback: function() {
-							window.location.href = "<?php echo base_url(); ?>edit/" + id;
-						}
-					},
-					Cancel: {
-						label: 'Cancel',
-						className: "btn-default",
-						callback: function() {
-							//nothing to do
-						}
-					}
-				}
-			});
-		});
-	});
+  <script src="<?php echo base_url(); ?>assets/js/jquery-2.2.0.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+  <script src="<?php echo base_url(); ?>assets/js/bootbox.min.js"></script>
+
+  <script>
+  $(document).ready(function() {
+    $('tbody tr').click( function() {
+      var id = $(this).children('td').html();
+      bootbox.dialog( {
+        title: 'Confirm',
+        message: 'Edit this entry?',
+        size: 'small',
+        animate: 'false',
+        onEscape: 'true',
+        buttons: {
+          OK: {
+            label: 'OK',
+            className: "btn-primary",
+            callback: function() {
+              window.location.href = "<?php echo base_url(); ?>edit/" + id;
+            }
+          },
+          Cancel: {
+            label: 'Cancel',
+            className: "btn-default",
+            callback: function() {
+              //nothing to do
+            }
+          }
+        }
+      });
+    });
+  });
 /*
-	$(document).ready(function() {
-		$('tbody tr').click( function() {
-			var r = confirm("Edit this record?");
-			if (r === true) {
-				window.location.href = "/expenses/main/edit/" + $(this).children('td').html();
-			}
-		});
-	});
+  $(document).ready(function() {
+    $('tbody tr').click( function() {
+      var r = confirm("Edit this record?");
+      if (r === true) {
+        window.location.href = "/expenses/main/edit/" + $(this).children('td').html();
+      }
+    });
+  });
 */
-	</script>
+  </script>
 
 </body>
 </html>
