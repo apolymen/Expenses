dfe465524c134fcd659fb65c60b03eaf4b7673c1
apolymen <apolymen@gmail.com>
2020-01-29 22:24:11 +0200

    added search functionality

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -20,7 +20,6 @@ class Main extends CI_Controller {
 	}
 
 	public function output() {
-    $st = "NIL";
 		//pagination settings
 		$config['base_url'] = site_url('output');
 		$config['per_page'] = $this->config->item('per_page'); //defined in main config
@@ -53,7 +52,7 @@ class Main extends CI_Controller {
 		$config['num_links'] = 2;
 
 		$data['currentpage'] = ($this->uri->segment(2)) ? $this->uri->segment(2) : 0;
-		$results = $this->model_expenses->get_records(max(0,($data['currentpage']-1))*$config['per_page'], $config['per_page'], $st);
+		$results = $this->model_expenses->get_records(max(0,($data['currentpage']-1))*$config['per_page'], $config['per_page'], NULL);
 		$data['expenses'] = $results->result();
 
 		$this->pagination->initialize($config);
@@ -61,6 +60,52 @@ class Main extends CI_Controller {
 
 		$this->load->view('view_retrieve', $data);
 	}
+
+	public function search() {
+    // get search string
+    $search = ($this->input->post("descr_search"))? $this->input->post("descr_search") : "NIL";
+    $search = ($this->uri->segment(2)) ? $this->uri->segment(2) : $search;
+
+		//pagination settings
+    $config = array();
+		$config['base_url'] = site_url('search/$search');
+		$config['per_page'] = $this->config->item('per_page'); //defined in main config
+		$config['uri_segment'] = 3;
+		$config['use_page_numbers'] = True;
+
+		//config for bootstrap pagination class integration
+		$config['full_tag_open'] = '<ul class="pagination">';
+		$config['full_tag_close'] = '</ul>';
+		$config['first_link'] = "First";
+		$config['last_link'] = "Last";
+		$config['first_tag_open'] = '<li>';
+		$config['first_tag_close'] = '</li>';
+		$config['prev_link'] = '&laquo';
+		$config['prev_tag_open'] = '<li class="prev">';
+		$config['prev_tag_close'] = '</li>';
+		$config['next_link'] = '&raquo';
+		$config['next_tag_open'] = '<li>';
+		$config['next_tag_close'] = '</li>';
+		$config['last_tag_open'] = '<li>';
+		$config['last_tag_close'] = '</li>';
+		$config['cur_tag_open'] = '<li class="active"><a href="#">';
+		$config['cur_tag_close'] = '</a></li>';
+		$config['num_tag_open'] = '<li>';
+		$config['num_tag_close'] = '</li>';
+
+		$config['total_rows'] = $this->model_expenses->get_records_count($search);
+		$data['per_page'] = $config['per_page'];
+		$config['num_links'] = 2;
+
+		$data['currentpage'] = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+		$results = $this->model_expenses->get_records(max(0,($data['currentpage']-1))*$config['per_page'], $config['per_page'], $search);
+		$data['expenses'] = $results->result();
+
+		$this->pagination->initialize($config);
+		$data['pagination'] = $this->pagination->create_links();
+
+		$this->load->view('view_retrieve', $data);
+  }
 
 	public function input() {
 		$data = $this->prepare_dropdowns();
