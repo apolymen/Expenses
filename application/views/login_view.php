d3389b5968142ebd41fd2c2841094e68d35be369
apolymen <apolymen@gmail.com>
2020-12-30 12:16:02 +0200

    Change login page title

--- application/views/login_view.php
+++ application/views/login_view.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login Form</title>
+    <title>Expenses Login</title>
 
     <!--link the bootstrap css file-->
 		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
