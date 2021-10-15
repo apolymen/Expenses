695f2801da65ab9ed86a98ae04081c50b52fd8e3
apolymen <apolymen@gmail.com>
2021-10-15 11:43:42 +0300

    Replace tabs with spaces

--- application/models/Model_expenses.php
+++ application/models/Model_expenses.php
@@ -4,67 +4,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // First letter of Model class and corresponding filename must be capital!
 class Model_expenses extends CI_Model {
 
-	public function __construct() {
-		parent::__construct();
+  public function __construct() {
+    parent::__construct();
         $this->load->database();
-	}
+  }
 
-	public function get_records($start, $limit, $st = NULL) {
-//		$query = $this->db->get('view_all');
+  public function get_records($start, $limit, $st = NULL) {
+//    $query = $this->db->get('view_all');
     if ($st == "NIL") $st = "";
-		$query = $this->db->select ('expdata.id AS id, xDate AS Date, Amount AS Amount, Person AS Person, Description AS Description')
-						->select ('paymentmethods.Method AS Method')
-						->select ('categories.Name AS Category')
-						->join ('paymentmethods',	'expdata.method_id = paymentmethods.id')
-						->join ('categories', 'expdata.category_id = categories.id')
+    $query = $this->db->select ('expdata.id AS id, xDate AS Date, Amount AS Amount, Person AS Person, Description AS Description')
+            ->select ('paymentmethods.Method AS Method')
+            ->select ('categories.Name AS Category')
+            ->join ('paymentmethods',  'expdata.method_id = paymentmethods.id')
+            ->join ('categories', 'expdata.category_id = categories.id')
             ->like ('Description', $st)
-						->order_by ('xDate ASC, id ASC')
-						->limit($limit, $start)
-						->get('expdata');
+            ->order_by ('xDate ASC, id ASC')
+            ->limit($limit, $start)
+            ->get('expdata');
 
-		if ($query->num_rows() > 0) {
-			return $query;
-		}
-		else {
-			return NULL;
-		}
-	}
+    if ($query->num_rows() > 0) {
+      return $query;
+    }
+    else {
+      return NULL;
+    }
+  }
 
   public function get_records_count($st = NULL) {
     if ($st == "NIL") $st = "";
-		$query = $this->db->like ('Description', $st)
-						->get('expdata');
+    $query = $this->db->like ('Description', $st)
+            ->get('expdata');
     return $query->num_rows();
   }
 
-	public function get_record_by_id($exp_id) {
-		$query = $this->db->select('expdata.id, expdata.xDate, expdata.amount, expdata.person, expdata.description')
-						->select('method_id AS payment, category_id AS category')
-						->where('expdata.id', $exp_id)
-						->get('expdata');
-		if ($query->num_rows() > 0) {
-			return $query;
-		}
-		else {
-			return NULL;
-		}
-	}
+  public function get_record_by_id($exp_id) {
+    $query = $this->db->select('expdata.id, expdata.xDate, expdata.amount, expdata.person, expdata.description')
+            ->select('method_id AS payment, category_id AS category')
+            ->where('expdata.id', $exp_id)
+            ->get('expdata');
+    if ($query->num_rows() > 0) {
+      return $query;
+    }
+    else {
+      return NULL;
+    }
+  }
   
-	public function add_record($data) {
-		$this->db->insert('expdata', $data);
-	}
+  public function add_record($data) {
+    $this->db->insert('expdata', $data);
+  }
 
-	public function update_record($id, $data) {
-		$this->db->where('id', $id);
-		$this->db->update('expdata', $data);
-	}
+  public function update_record($id, $data) {
+    $this->db->where('id', $id);
+    $this->db->update('expdata', $data);
+  }
 
-	public function delete_record($id) {
-		$this->db->where('id', $id);
-		$this->db->delete('expdata');
-	}
+  public function delete_record($id) {
+    $this->db->where('id', $id);
+    $this->db->delete('expdata');
+  }
 
-	// Get paymentmethods table to populate the "Payment by" dropdown
+  // Get paymentmethods table to populate the "Payment by" dropdown
     function get_paymentmethods()
     {
         $this->db->select('id');
@@ -85,7 +85,7 @@ class Model_expenses extends CI_Model {
         return $paymentmethods_result = array_combine($pay_id, $pay_name);
     }
 
-	// Get categories table to populate the Category dropdown
+  // Get categories table to populate the Category dropdown
     function get_categories()
     {
         $this->db->select('id');
